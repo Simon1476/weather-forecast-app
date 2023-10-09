9fac5adf7282cffa33420d6b9e69b08736f29ca1
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-10-09 11:39:32 +0900

    Remove redundant variables and include sunset data

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,5 @@
 const API_KEY = "aeaf790446d5d5bd11a6d4eff7e273c2";
 
-const searchBar = document.querySelector(".search-bar input");
-const todayImg = document.querySelector(".today-img");
-
 const getCoords = async (city = "Seoul") => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
@@ -20,23 +17,20 @@ const getTodayWeather = async (lat, lon, unit = "metric") => {
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${unit}`
   );
   const result = await response.json();
+
   const {
-    main: { temp_max, temp_min, feels_like, humidity },
+    main: { feels_like, humidity },
     name,
     wind: { speed },
-    sys: {},
+    sys: { sunset },
   } = result;
 
-  const iconcode = result.weather[0].icon;
-
   const weatherData = {
-    temperatureMax: temp_max,
-    temperatureMin: temp_min,
     feelsLike: feels_like,
     humidity: humidity,
     cityName: name,
     windSpeed: speed,
-    iconcode: iconcode,
+    sunset: sunset,
   };
 
   return weatherData;
@@ -47,7 +41,6 @@ const getforeCast = async (lat, lon, unit = "metric") => {
     `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}4&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Asia%2FTokyo`
   );
   const result = await response.json();
-  console.log(result);
   const {
     daily: {
       temperature_2m_max: maxtmp,
