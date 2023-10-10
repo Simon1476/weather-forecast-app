3bd5b29c4e720fbf0b86011a8576387c8dc4830e
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-10-10 11:46:50 +0900

    Set default search term Seoul

--- js/gifs.js
+++ js/gifs.js
@@ -1,6 +1,6 @@
-const getGifUrl = async () => {
+const getGifUrl = async (searchTerm = "Seoul") => {
   const response = await fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=iFooQPDHMmF6NnBtd6fxQNLXW9xg0DI2&s=cats",
+    `https://api.giphy.com/v1/gifs/translate?api_key=iFooQPDHMmF6NnBtd6fxQNLXW9xg0DI2&s=${searchTerm}`,
     {
       mode: "cors",
     }
